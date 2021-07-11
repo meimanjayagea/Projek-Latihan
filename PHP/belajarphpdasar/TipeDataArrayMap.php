c40a7836fb9bf1bc78ec83707e48728b145315db
Mas Gea <jayameiman17@gmail.com>
2021-07-11 13:15:35 +0700

    Tipedata Array, map, Ardlam array, null,constant

---
+++ PHP/belajarphpdasar/TipeDataArrayMap.php
@@ -0,0 +1,19 @@
+<?php
+//array MAP
+$meiman = array(
+    "id" => "Meiman",
+    "name" => "Meiman Jaya Gea",
+    "age" => 26
+);
+
+var_dump($meiman);
+var_dump($meiman["name"]);
+
+$budi = [
+    "id" => "budi",
+    "name" => "Budi Kurnia",
+    "age" => 26
+];
+
+var_dump($budi);
+var_dump($budi["name"]);
